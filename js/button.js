9f4572d3360187dcc9c04f96af7a980b6b614644
quantxz <indiedev.core@gmail.com>
2024-04-20 09:05:48 -0300

    rsposive site init

---
+++ js/button.js
@@ -0,0 +1,44 @@
+class ButtonFunctions {
+
+    async requisitionRender() {
+        const url = "https://gf-db-transfer-quantxz.vercel.app";
+        const urlPath = document.querySelector(".urlPath").value;
+        resultDiv.textContent = ''
+        try {
+            const result = await fetch(`${url}${urlPath}`);
+
+            if (!result.ok) {
+                throw new Error('Erro ao realizar a requisição');
+            }
+            const data = await result.json();
+            resultDiv.textContent = '';
+
+
+            data.forEach(obj => {
+                const objDiv = document.createElement('div');
+                objDiv.textContent = JSON.stringify(obj, null, 4);
+                resultDiv.appendChild(objDiv);
+            });
+
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    async copyText() {
+        const url = "https://gf-db-transfer-quantxz.vercel.app";
+        const urlPath = document.querySelector(".urlPath").value;
+        const requisition = `${url}${urlPath}`
+
+        navigator.clipboard.writeText(requisition)
+            .then(() => {
+                console.log('Texto copiado com sucesso!');
+                alert('Texto copiado com sucesso!');
+            })
+            .catch(err => {
+                console.error('Erro ao copiar texto:', err);
+                alert('Erro ao copiar texto. Por favor, copie manualmente.');
+            });
+    }
+
+}
--- js/main.js
+++ js/main.js
@@ -1,27 +1,24 @@
 const requireButton = document.querySelector(".requireButton");
+const copyButton = document.querySelector(".copyRequisition")
 const resultDiv = document.querySelector(".result");
+const buttonsFunctions = new ButtonFunctions();
+let btnMenu = document.querySelector(".btn-abrir-menu")
+let menuContent = document.querySelector(".menu-mobile")
+let overlayMenu = document.querySelector(".overlay-menu")
 
-requireButton.addEventListener("click", async () => {
-    const url = "https://gf-db-transfer-quantxz.vercel.app";
-    const urlPath = document.querySelector(".urlPath").value;
-    resultDiv.textContent = ''
-    try {
-        const result = await fetch(`${url}${urlPath}`);
-        
-        if (!result.ok) {
-            throw new Error('Erro ao realizar a requisição');
-        }
-        const data = await result.json();
-        resultDiv.innerHTML = '';
+requireButton.addEventListener("click", buttonsFunctions.requisitionRender)
+copyButton.addEventListener("click", buttonsFunctions.copyText)
 
-        
-        data.forEach(obj => {
-            const objDiv = document.createElement('div');
-            objDiv.textContent = JSON.stringify(obj, null, 4);
-            resultDiv.appendChild(objDiv);
-        });
 
-    } catch (error) {
-        console.error(error);
-    }
+btnMenu.addEventListener("click", () => {
+    menuContent.classList.add("abrir-menu")
+    console.log("clicoou")
+})
+
+menuContent.addEventListener("click", () => {
+    menuContent.classList.remove("abrir-menu")
+})
+
+overlayMenu.addEventListener("click", () => {
+    menuContent.classList.remove("abrir-menu")
 })
