50288a5691eaf27843d5d55ad20c7bc7c8bd07f6
quantxz <indiedev.core@gmail.com>
2024-04-22 14:33:50 -0300

    responsivity bug fix

--- js/style.js
+++ js/style.js
@@ -1,4 +1,19 @@
 window.sr = ScrollReveal({ reset: true });
+const contentLink = document.querySelector("#contentLink");
+
+document.addEventListener('scroll', () => {
+    document.createEvent()
+  });
+
+
+document.getElementById("contentLink").addEventListener("click",() => {
+    window.scrollTo(0, document.body.scrollHeight);
+    const divFlex = document.querySelector(".tryNow > .flex");
+    divFlex.style = "background-color: rgba(122, 122, 122, 0.6);  transition: 1s;"
+    setTimeout(() => {
+        divFlex.style = "background-color: transparent; transition: 1s;"
+    }, 500);
+})
 
 const divsForReveal = [tryNowTitle, apresentacaoTitle, apresentacaoP, inputRequire, result] = [
     document.querySelector(".tryNow > .flex > .title"),
